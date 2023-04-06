36690f4a26aa63799a64783ad45c21979e997fc7
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-04-06 19:07:38 -0400

    Fix spacing

--- .github/scripts/request.sh
+++ .github/scripts/request.sh
@@ -15,15 +15,15 @@ mkdir -p generated
 docker manifest inspect "$CONTAINER"
 
 cat << EOF >> "generated/workshop-values-$ID.yaml"
-/galaxy/server/tools/interactive/biocworkshop_$ID.xml:
-  useSecret: false
-  applyToJob: true
-  applyToSetupJob: true
-  applyToWeb: true
-  applyToWorkflow: true
-  applyToNginx: true
-  tpl: false
-  content: |
+  /galaxy/server/tools/interactive/biocworkshop_$ID.xml:
+    useSecret: false
+    applyToJob: true
+    applyToSetupJob: true
+    applyToWeb: true
+    applyToWorkflow: true
+    applyToNginx: true
+    tpl: false
+    content: |
 $(sed """s@##PLACEHOLDERID##@${ID}@g
        s@##PLACEHOLDERNAME##@${TITLE}@g
        s@##PLACEHOLDERDESCRIPTION##@${DESCRIPTION}@g
