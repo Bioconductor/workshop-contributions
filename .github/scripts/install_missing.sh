5153be23c2c5b7374056765a2ff8a7bdac78c0f7
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-04-12 21:58:53 -0400

    Update install_missing.sh

--- .github/scripts/install_missing.sh
+++ .github/scripts/install_missing.sh
@@ -4,5 +4,5 @@ if [ ! -f ../install_missing.sh ]
 then
     ( cd .. & curl -o install_missing.sh https://raw.githubusercontent.com/Bioconductor/BiocDeployableQuarto/main/.github/scripts/install_missing.sh )
 fi
-cp -r "$TOCOPY" /home/rstudio/
-find "$TOCOPY" -type f -name "*.*md" -print0 | xargs -r0 -i bash ../install_missing.sh {}
+cp -r $TOCOPY /home/rstudio/
+find $TOCOPY -type f -name "*.*md" -print0 | xargs -r0 -i bash ../install_missing.sh {}
