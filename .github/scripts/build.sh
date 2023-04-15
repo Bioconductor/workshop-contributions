3800eaaa7a6852639777bd220ee1e0b590605665
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-04-14 20:53:39 -0400

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -5,6 +5,8 @@ set -xe
 ID=$1
 CONTAINER=$(cat generated/$ID.container)
 
+cat generated/$ID.Dockerfile
+
 docker manifest inspect "$CONTAINER" && ( echo "Container found" ) || \
  ( echo "Container not found. Building" &&\
    docker build . -f generated/$ID.Dockerfile -t $CONTAINER &&\
