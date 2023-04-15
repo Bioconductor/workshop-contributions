aaed1ad7f8550c461a0182533048c8078842835a
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-04-14 20:44:01 -0400

    Remove unused env

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -4,7 +4,6 @@ set -xe
 
 ID=$1
 CONTAINER=$(cat generated/$ID.container)
-BIOCVER="3.16"
 
 docker manifest inspect "$CONTAINER" && ( echo "Container found" ) || \
  ( echo "Container not found. Building" &&\
