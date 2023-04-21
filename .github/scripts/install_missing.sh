3d8e304da638f9fa4e2a6afa967dbbf7a939b206
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-04-21 14:21:45 -0400

    Update install_missing.sh

--- .github/scripts/install_missing.sh
+++ .github/scripts/install_missing.sh
@@ -4,5 +4,5 @@ if [ ! -f "install_missing.sh" ]
 then
     curl -o install_missing.sh https://raw.githubusercontent.com/Bioconductor/BiocDeployableQuarto/main/.github/scripts/install_missing.sh
 fi
-mkdir -p /home/rstudio/workshop
+
 find $TORENDER -type f -name "*.*md" -print0 | xargs -r0 -i bash install_missing.sh {}
