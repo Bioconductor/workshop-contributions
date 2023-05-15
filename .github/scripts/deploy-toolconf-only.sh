662ac738024a26e217732ca9e8e6317e8c792ee4
Alexandru Mahmoud <mahmoudalexandru@gmail.com>
2023-05-15 19:24:37 -0400

    Create deploy-toolconf-only.sh

---
+++ .github/scripts/deploy-toolconf-only.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -xe
+
+NAMESPACE="gxy-bioc"
+GXYRELEASE="gxy"
+CHARTVER="4.10.2"
+
+helm repo add bioc https://github.com/Bioconductor/helm-charts/raw/devel
+helm repo update
+helm upgrade --wait --timeout 600s --install --create-namespace -n $NAMESPACE $GXYRELEASE bioc/galaxy --version $CHARTVER --reuse-values -f generated/workshop-toolconf-values.yaml
