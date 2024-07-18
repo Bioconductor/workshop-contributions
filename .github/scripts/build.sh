80c6bd45595820a02feb17dade778d6739a7a966
Alexandru Mahmoud <almahmoudresearch@gmail.com>
2024-07-18 13:05:05 -0400

    Try docker instead of GHCR

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -4,10 +4,11 @@ set -xe
 
 ID=$1
 CONTAINER=$(cat generated/$ID.container)
+DOCKERCONTAINER=$(echo $CONTAINER | sed 's/ghcr.io/docker.io/g')
 
 
 docker buildx imagetools inspect "$CONTAINER" && ( echo "Container found" ) || \
  ( echo "Container not found. Building" && cat generated/$ID.Dockerfile && \
-   docker build . -f generated/$ID.Dockerfile -t $CONTAINER &&\
-   docker push $CONTAINER )
+   docker build . -f generated/$ID.Dockerfile -t $CONTAINER -t $DOCKERCONTAINER &&\
+   docker push $CONTAINER || ( docker push $DOCKERCONTAINER && echo $DOCKERCONTAINER > generated/$ID.container && git add generated/$ID.container && git commit -m "Container $ID in docker instead" && git push ) )
 
